a6abf9a4efc10a30a050f47ad6996360e30e1334
jessicaidro <jessicaidro@gmail.com>
2021-08-23 21:34:13 -0300

    Aula 04
    Binds

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -10,6 +10,7 @@ class FormularioCadastro extends Component {
 
   handleMudancaTitulo(evento) {
     this.titulo = evento.target.value;
+    console.log(this.titulo);
   }
 
   render() {
@@ -19,7 +20,7 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudancaTitulo}
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
