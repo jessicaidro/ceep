58a169172ee5abf21d998f9154e2d31a5e6c2361
jessicaidro <jessicaidro@gmail.com>
2021-08-24 23:20:53 -0300

    Aula 5 - Final
        Pensando sobre como transmitir informações entre objetos
    
        Fluxo de atualização do sistema
    
        Para que serve o state
    
        Como atribuir o state de maneira segura
    
        Atualizando a visualização da tela dinamicamente

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import "./assets/app.css";
 import './assets/index.css';
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.notas = [];
+        this.state = {
+            notas:[]
+        };
+    }
+    criarNota(titulo, texto){
+        const novaNota = {titulo, texto};
+        const novoArrayNotas = [...this.state.notas,novaNota]
+        const novoEstado = {
+            notas:novoArrayNotas
+        };
+        this.setState(novoEstado);
+    }
+
     render() {
+        
         return ( 
             <section className="conteudo">
-                <FormularioCadastro/>
-                <ListaDeNotas/>
+                <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+                <ListaDeNotas notas={this.state.notas}/>
         </section>
         );
     }
